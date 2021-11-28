e4ba25d0163e65b1a6f72f16ab6cf0a1e7d85afd
ushathakur <55362982+ushathakur@users.noreply.github.com>
2021-11-27 21:44:44 -0500

    store tabs and cooresponding click count in local storage

--- background.js
+++ background.js
@@ -4,16 +4,17 @@ chrome.runtime.onMessage.addListener(
     /*console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");*/
-    chrome.storage.sync.set({ tab: { url: sender.tab.url, count: request.count } }, function () {
-      console.log('the current tab is:' + sender.tab.url + 'with click count:' + request.count);
+    chrome.storage.sync.set({ [sender.tab.title]: [request.count]}, function () {
+      console.log('stored data. The current tab is:' + sender.tab.title + 'with click count:' + request.count);
     });
+    /*
     chrome.storage.sync.get(['tab'], function (result) {
       console.log('the current tab is:' + result.tab.url + 'with click count:' + result.tab.count);
-    });
+    });*/
     chrome.storage.sync.get(null, function (items) {
       var allKeys = Object.keys(items);
       console.log(allKeys);//get all keys in local storage
-      console.log(items.tab);
+      console.log(items);
     });
  
     sendResponse({ farewell: "goodbye" });
--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
-chrome.storage.sync.get(null,(result)=>{
+chrome.storage.sync.get(null,(items)=>{
+    
+    var timeSpent = Object.values(items);
+    var maxTimeSpent = Math.max.apply(null, timeSpent);
+    console.log(maxTimeSpent);
+    const topOneTab = Object.keys(items).find(key => items[key] == maxTimeSpent);
+    console.log(topOneTab);
+
+
     let pElement = document.createElement('p');
-    let pElementText =document.createTextNode(`tab is : ${result.tab.url}, count is: ${result.tab.count}`)
+    let pElementText =document.createTextNode(`The most clicked tab is : ${topOneTab}\n,  clicks: ${maxTimeSpent}`);
     pElement.appendChild(pElementText);
     let parent = document.querySelector('.container');
     parent.appendChild(pElement);
-    
-   
-   
-    
 });
+
+
+// @sourceURL=popup.js
